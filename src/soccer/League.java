3a33f10764e9d17c546af41b70dd4cabec6c3c4d
MinenhleS <ngwenyasiyamukela@gmail.com>
2019-07-25 12:28:50 +0200

    first commit

--- src/soccer/League.java
+++ src/soccer/League.java
@@ -1,10 +1,19 @@
 package soccer;
 
+import java.util.*;
+
 public class League {
 
     public static void main(String[] args) {
 
-    // Declaring and instantiating a first player and set its playerName
+
+        Team[] theTeams = createTeams();
+
+        Game[] theGames = createGames(theTeams);
+
+
+
+        // Declaring and instantiating a first player and set its playerName
         Player player1 = new Player();
         player1.playerName = "George Eliot";
 
@@ -22,33 +31,32 @@ public class League {
         Player[] thePlayers = { player1, player2, player3 };
 
         //Declaring and instatiating Team object
-        Team team1 = new Team();
-        Team team2 = new Team();
+
 
         //set teamName
-        team1.teamName = "The Greens";
-        team2.teamName = "The Reds";
+        theTeams[0].teamName = "The Greens";
+        theTeams[1].teamName = "The Reds";
 
         //set the playerArray attribute of the Team to thePlayers
-        team1.playerArray = thePlayers;
+        theTeams[0].playerArray = thePlayers;
 
         //Creating 3 element array and assigning it to playeyerArray
-        team2.playerArray = new Player[3];
+        theTeams[1].playerArray = new Player[3];
 
         //Add player to the 1st element
-        team2.playerArray[0] = new Player();
-        team2.playerArray[0].playerName = "Robert Service";
+        theTeams[1].playerArray[0] = new Player();
+        theTeams[1].playerArray[0].playerName = "Robert Service";
 
 
         //Adding two players to the second and third element
-        team2.playerArray[1] = new Player();
-        team2.playerArray[1].playerName = "Robbie Burns";
-        team2.playerArray[2] = new Player();
-        team2.playerArray[2].playerName = "Rafael Sabatini";
+        theTeams[1].playerArray[1] = new Player();
+        theTeams[1].playerArray[1].playerName = "Robbie Burns";
+        theTeams[1].playerArray[2] = new Player();
+        theTeams[1].playerArray[2].playerName = "Rafael Sabatini";
 
 
 
-        for (Player thePlayer: team2.playerArray) {
+        for (Player thePlayer: theTeams[1].playerArray) {
             System.out.println(thePlayer.playerName);
         }
 
@@ -56,29 +64,36 @@ public class League {
 
 
         // Creating and populating  home and away team attributes
-        Game currGame = new Game();
-        currGame.homeTeam = team1;
-        currGame.awayTeam = team2;
+        Game currGame = theGames[0];
 
-        // Creating goal object
-        Goal goal1 = new Goal();
-        goal1.thePlayer = currGame.homeTeam.playerArray[2];
-        goal1.theTeam = currGame.homeTeam;
-        goal1.theTime = 55;
+        int numberOfGoals = (int)(Math.random() * 7);
+        Goal[] theGoals = new Goal[numberOfGoals];
+        //System.out.println(theGoals.length);
 
-        //putting goal object to the array
-        Goal[] theGoals = {goal1};
+
         currGame.goals = theGoals;
 
-        //Printing the Score
-        System.out.println("Goal scored after " +
-                currGame.goals[0].theTime + " mins by " +
-                currGame.goals[0].thePlayer.playerName + " of " +
-                currGame.goals[0].theTeam.teamName);
+        GameUtils.addGameGoals(currGame);
+
+        // Creating goal object
+//        Goal goal1 = new Goal();
+//        goal1.thePlayer = currGame.homeTeam.playerArray[2];
+//        goal1.theTeam = currGame.homeTeam;
+//        goal1.theTime = 55;
+//
+//        //putting goal object to the array
+//        Goal[] theGoals = {goal1};
+//        currGame.goals = theGoals;
+
+//        //Printing the Score
+//        System.out.println("Goal scored after " +
+//                currGame.goals[0].theTime + " mins by " +
+//                currGame.goals[0].thePlayer.playerName + " of " +
+//                currGame.goals[0].theTeam.teamName);
 
 
         //Testing whether a match is found for "Sab"
-        for (Player thePlayer: team2.playerArray) {
+        for (Player thePlayer: theTeams[1].playerArray) {
             if (thePlayer.playerName.matches(".*Sab.*")) {
                 System.out.println("Found " + thePlayer.playerName);
                 System.out.println("Last name is " +
@@ -86,8 +101,44 @@ public class League {
             }
         }
 
+        //Instatiating string builder
+        StringBuilder familyNameFirst = new StringBuilder();
+
+        for (Player thePlayer: theTeams[0].playerArray) {
+            String name[] = thePlayer.playerName.split(" ");
+            familyNameFirst.append(name[1]);
+            familyNameFirst.append(", ");
+            familyNameFirst.append(name[0]);
+
+            System.out.println(familyNameFirst);
+            familyNameFirst.delete(0, familyNameFirst.length());
+        }
+
+
+
 
 
 
     }
+
+
+    public static Team[] createTeams() {
+        Team team1 = new Team();
+        Team team2 = new Team();
+
+        Team[] theTeams = {team1, team2};
+        return theTeams;
+    }
+
+    public static Game[] createGames(Team[] theTeams) {
+
+        Game theGame = new Game();
+        theGame.homeTeam = theTeams[0];
+        theGame.awayTeam = theTeams[1];
+
+        Game[] theGames = {theGame};
+        return theGames;
+
+    }
+
 }
